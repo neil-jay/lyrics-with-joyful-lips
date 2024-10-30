4c770b7f41aae1de6a944ec20de33f8e7e856ca5
neil-jay <neiljay@pcmender.online>
2024-10-30 21:51:22 +0530

    Added layout.js for JSON-LD structured data

---
+++ src/theme/Layout.js
@@ -0,0 +1,24 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "With Joyful Lips Lyrics",
+  "url": "https://withjoyfullipslyrics.neiljay.com",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://withjoyfullipslyrics.neiljay.com/search?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+},
+  "author": {
+    "@type": "Person",
+    "name": "Fr. Jean L. Mercier"
+  },
+  "about": {
+    "@type": "Book",
+    "name": "With Joyful Lips",
+    "author": "Fr. Jean L. Mercier",
+    "bookEdition": "Twelfth Revised Edition",
+    "bookFormat": "Hymnal"
+  }
+}
+</script>
