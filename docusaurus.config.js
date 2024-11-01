df822501c116c67bc653929e0d40e7effb3e89c1
neil-jay <neiljay@pcmender.online>
2024-11-01 22:23:29 +0530

    Added sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,6 +66,12 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
       }),
     ],
   ],
